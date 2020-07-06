b5154ad31b57fc328a6461b49d1463fcd7ba7e52
preich <preich@192.168.1.101>
2020-07-06 18:39:56 +0200

    fetch dependencies classpath

--- src/main/java/com/github/jazzmuesli/ckjm/plugin/CKJMMetricsMojo.java
+++ src/main/java/com/github/jazzmuesli/ckjm/plugin/CKJMMetricsMojo.java
@@ -2,22 +2,35 @@ package com.github.jazzmuesli.ckjm.plugin;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.bcel.util.ClassPath;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
+import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.repository.RepositorySystem;
+import org.codehaus.plexus.classworlds.ClassWorld;
 import org.codehaus.plexus.classworlds.realm.ClassRealm;
 
 import gr.spinellis.ckjm.MetricsFilter;
@@ -30,6 +43,15 @@ public class CKJMMetricsMojo extends AbstractMojo {
 
 	@Component
 	private PluginDescriptor pluginDescriptor;
+	
+	@Parameter(defaultValue = "${plugin.artifactMap}", required = true, readonly = true)
+	private Map<String, Artifact> pluginArtifactMap;
+	
+	@Parameter(defaultValue = "${localRepository}", required = true, readonly = true)
+	private ArtifactRepository localRepository;
+
+	@Component
+	private RepositorySystem repositorySystem;
 
 	public void execute() throws MojoExecutionException {
 		File buildDirectory = new File(project.getBuild().getOutputDirectory());
@@ -46,7 +68,11 @@ public class CKJMMetricsMojo extends AbstractMojo {
 			getLog().error(e1);
 		}
 
-		ClassPath cp = ClassPath.SYSTEM_CLASS_PATH;
+		// this is a pretty ugly way to get classpath of dependencies.
+    	LinkedHashSet<String> classpath = prepareClasspath(project, localRepository,
+				repositorySystem, pluginArtifactMap, getLog());
+
+		ClassPath cp = new ClassPath(classpath.stream().collect(Collectors.joining(File.pathSeparator)));
         if (new File(project.getBuild().getOutputDirectory()).exists()) {
         	// use output dir or delegate to previous cp
         	cp = new ClassPath(cp, project.getBuild().getOutputDirectory());
@@ -61,6 +87,88 @@ public class CKJMMetricsMojo extends AbstractMojo {
 		processSourceDirectory(cp, project.getBuild().getTestOutputDirectory());
 	}
 
+	private static final String CTESTER_PACKAGE = "com.github.jazzmuesli:ckjm-mvn-plugin";
+
+	private static Set<Artifact> getDevArtifacts(ArtifactRepository localRepository, RepositorySystem repositorySystem,
+			Map<String, Artifact> pluginArtifactMap) {
+		ArtifactResolutionRequest request = new ArtifactResolutionRequest()
+				.setArtifact(pluginArtifactMap.get(CTESTER_PACKAGE)).setResolveTransitively(true)
+				.setLocalRepository(localRepository);
+		ArtifactResolutionResult result = repositorySystem.resolve(request);
+		return result.getArtifacts();
+	}
+
+	/**
+	 * TODO: make it less hacky.
+	 * 
+	 * Here we need classpath of the target project + target/test-clases,
+	 * target/classes + dependencies of this plugin. Note that this plugin won't be
+	 * added to target project's pom.xml, but rather executed mvn
+	 * org.pavelreich.saaremaa:plugin:analyse
+	 * 
+	 * @param log
+	 * @param project
+	 * @param pluginArtifactMap
+	 * @param repositorySystem
+	 * @param localRepository
+	 * @return
+	 * @throws MojoExecutionException
+	 */
+	public static LinkedHashSet<String> prepareClasspath(MavenProject project, ArtifactRepository localRepository,
+			RepositorySystem repositorySystem, Map<String, Artifact> pluginArtifactMap, Log log)
+			throws MojoExecutionException {
+		LinkedHashSet<String> classpath = new LinkedHashSet<String>();
+
+		try {
+			log.info("project: "+project);
+			project.getArtifacts().stream().filter(x -> x.getGroupId().equals(project.getGroupId())).forEach(x -> log.info("artifact: " + x));
+			classpath.addAll(project.getTestClasspathElements());
+			
+			classpath.addAll(project.getCompileClasspathElements());
+			// copied from
+			// https://github.com/tbroyer/gwt-maven-plugin/blob/54fe4621d1ee5127b14030f6e1462de44bace901/src/main/java/net/ltgt/gwt/maven/CompileMojo.java#L295
+			ClassWorld world = new ClassWorld();
+			ClassRealm realm;
+			log.info("pluginArtifactMap:" + pluginArtifactMap.keySet());
+			try {
+				realm = world.newRealm("gwt", null);
+				for (String elt : project.getCompileSourceRoots()) {
+					URL url = new File(elt).toURI().toURL();
+					realm.addURL(url);
+				}
+				for (String elt : project.getCompileClasspathElements()) {
+					URL url = new File(elt).toURI().toURL();
+					realm.addURL(url);
+				}
+				for (Artifact elt : getDevArtifacts(localRepository, repositorySystem, pluginArtifactMap)) {
+					URL url = elt.getFile().toURI().toURL();
+					realm.addURL(url);
+					log.info("transitive classpath: " + url);
+				}
+				URL pluginUrls = pluginArtifactMap.get(CTESTER_PACKAGE).getFile().toURI().toURL();
+				realm.addURL(pluginUrls);
+				List<String> urls = Arrays.asList(realm.getURLs()).stream().map(x -> {
+					try {
+						return new File(x.toURI()).getAbsolutePath();
+					} catch (Exception e) {
+						throw new IllegalArgumentException(e.getMessage(), e);
+					}
+				}).collect(Collectors.toList());
+				urls.stream().filter(p->isAcceptedDependency(p)).forEach(x -> classpath.add(x));
+			} catch (Exception e) {
+				throw new MojoExecutionException(e.getMessage(), e);
+			}
+
+		} catch (DependencyResolutionRequiredException e1) {
+			log.error(e1.getMessage(), e1);
+		}
+		return classpath;
+	}
+
+	private static boolean isAcceptedDependency(String p) {
+		return true;
+	}
+	
 	protected void processSourceDirectory(ClassPath cp, String dirName) {
 		try {
 			getLog().info("Processing " + dirName);
